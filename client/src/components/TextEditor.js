d53032eb11a6e635d067623b664b320fd71a3e18
Igor Nikiforov <desola2@inbox.ru>
2021-10-03 21:25:33 +0300

    Add socket.io deps, connection, add nodemon

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,21 +1,30 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Quill from 'quill';
+import { io } from 'socket.io-client';
 import 'quill/dist/quill.snow.css';
 import '../scss/styles.scss';
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
-  [{ list: "ordered" }, { list: "bullet" }],
-  ["bold", "italic", "underline"],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
   [{ color: [] }, { background: [] }],
-  [{ script: "sub" }, { script: "super" }],
+  [{ script: 'sub' }, { script: 'super' }],
   [{ align: [] }],
-  ["image", "blockquote", "code-block"],
-  ["clean"],
-]
+  ['image', 'blockquote', 'code-block'],
+  ['clean']
+];
 
 const TextEditor = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   const wrapperRef = useCallback(wrapper => {
     if (wrapper === null) return;
 
@@ -25,9 +34,9 @@ const TextEditor = () => {
 
     new Quill(editor, {
       theme: 'snow',
-			modules: {
-				toolbar: TOOLBAR_OPTIONS
-			}
+      modules: {
+        toolbar: TOOLBAR_OPTIONS
+      }
     });
   }, []);
 
